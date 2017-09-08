e4dbcc57a18bd9617dfd0dac088d800c023b1f29
Grady L Shelton <gradyish@gmail.com>
2017-09-08 08:52:20 -0700

    write and pass model tests for users and emission estimates. prepare for integration testing

---
+++ spec/models/co2_estimate_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Co2Estimate do
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :nat_gas}
+  it { should validate_presence_of :fuel_oil }
+  it { should validate_presence_of :electricity }
+  it { should validate_presence_of :mode }
+  it { should validate_presence_of :miles }
+  it { should validate_presence_of :alum_re }
+  it { should validate_presence_of :glass_re }
+  it { should validate_presence_of :paper_re }
+  it { should validate_presence_of :lamb }
+  it { should validate_presence_of :beef }
+  it { should validate_presence_of :cheese }
+  it { should validate_presence_of :pork }
+  it { should validate_presence_of :turkey }
+  it { should validate_presence_of :chicken }
+  it { should validate_presence_of :tuna }
+  it { should validate_presence_of :eggs }
+  it { should validate_presence_of :potatoes }
+  it { should validate_presence_of :rice }
+  it { should validate_presence_of :nuts }
+  it { should validate_presence_of :beans }
+  it { should validate_presence_of :vegetables }
+  it { should validate_presence_of :milk }
+  it { should validate_presence_of :fruit }
+  it { should validate_presence_of :lentils }
+  it { should validate_presence_of :plastic_re }
+
+
+
+
+end
