70443e4e7f7c460ee3ca20074a77f808b6da0546
Grady Shelton <gradyish@gmail.com>
2018-04-08 19:48:00 -0700

    adjust indentation of factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -36,29 +36,29 @@ FactoryGirl.define do
     association :user, :factory => :admin_user, strategy: :build
     sequence(:monthly_emittance) {|n| n * (Faker::Number.between(70, 500)) }
     nat_gas 30
-     fuel_oil 0
-     sequence(:electricity) { |n| n*100 }
-     mode 'sedan'
-     sequence(:miles) { |n| n+100 }
-     sequence(:lamb) { |n| n }
-     sequence(:beef) { |n| n+1 }
-     cheese 2
-     pork 2
-     turkey 2
-     sequence(:chicken) { |n| n+2 }
-     tuna 3
-     eggs 4
-     potatoes 5
-     rice 2
-     nuts 1
-     beans 1
-     vegetables 3
-     milk 0
-     fruit 2
-     lentils 0
-     plastic_re 'yes'
-     alum_re 'yes'
-     glass_re 'yes'
-     paper_re 'yes'
+    fuel_oil 0
+    sequence(:electricity) { |n| n*100 }
+    mode 'sedan'
+    sequence(:miles) { |n| n+100 }
+    sequence(:lamb) { |n| n }
+    sequence(:beef) { |n| n+1 }
+    cheese 2
+    pork 2
+    turkey 2
+    sequence(:chicken) { |n| n+2 }
+    tuna 3
+    eggs 4
+    potatoes 5
+    rice 2
+    nuts 1
+    beans 1
+    vegetables 3
+    milk 0
+    fruit 2
+    lentils 0
+    plastic_re 'yes'
+    alum_re 'yes'
+    glass_re 'yes'
+    paper_re 'yes'
   end
 end
